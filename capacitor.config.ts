df73f9438c2bf397e16017ccb6ec137a4fbf144d
AJ Speller <ajspeller@gmail.com>
2022-01-23 23:20:43 -0500

    Add app skeleton and packages

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.certify-yaself.giflist',
+  appName: 'giflist',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
